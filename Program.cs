c2d4d17018c310d13835236a1773cf5ab5abe0d5
Rob Little <rlittletht@users.noreply.github.com>
2020-01-15 16:19:00 -0800

    Initial hello world

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
